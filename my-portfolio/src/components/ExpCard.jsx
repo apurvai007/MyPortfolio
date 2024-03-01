fe846de1c337610623a35a758bca5233c5ade642
Apurva Mathur <mathurapurva007@gmail.com>
2024-03-01 21:05:32 +0530

    company card

--- my-portfolio/src/components/ExpCard.jsx
+++ my-portfolio/src/components/ExpCard.jsx
@@ -1,27 +1,48 @@
+// ExpCard.js
+import React, { useState } from "react";
 import moment from "moment";
-import React from "react";
 import { CountUp } from "use-count-up";
+import styles from "@/styles/ExpCard.module.css";
+import cimpress from "../assets/images/cimpress.svg"
+import Image from "next/image";
 
 export default function ExpCard() {
+  const [isHovered, setIsHovered] = useState(false);
   const getExp = () => {
     let years = moment().diff(moment([2022, 1, 1]), "years");
     return years;
   };
 
+  const handleHover = () => {
+    setIsHovered(!isHovered);
+  };
+
   return (
-    <div className="text-white rounded-3xl flex relative overflow-hidden aspect-square shadow-sm bg-blue-400/80 p-4 justify-center items-center">
-      <div className="">
-        <div className="flex justify-center items-center flex-col">
-          <p className="text-4xl md:text-8xl font-bold">
-            <CountUp isCounting end={getExp()} duration={6} />+
-          </p>
-          <p className="text-xs md:text-xl uppercase font-semibold text-center">
-            Years of
-          </p>
-          <p className="text-xs md:text-xl uppercase font-semibold text-center">
-            Work experience
-          </p>
-        </div>
+    <div
+      className={`${styles.expCard} ${isHovered ? styles.hovered : ""}`}
+      onMouseEnter={handleHover}
+      onMouseLeave={handleHover}
+    >
+      <div className="flex justify-center items-center flex-col">
+      {isHovered ? (
+            <div className="w-32 md:w-32 relative">
+              <Image
+                src= {cimpress}
+                alt="Cimpress (company logo)"
+                className="object-cover w-full h-auto transform scale-150"
+              />
+            </div>
+          ) : (
+            <p className="text-4xl md:text-8xl font-bold">
+              <CountUp isCounting end={getExp()} duration={2} />+
+            </p>
+          )}
+        <p className="text-xs md:text-xl uppercase font-semibold text-center">
+          {isHovered ? "Worked here" : "Years of"}
+        </p>
+        <p className="text-xs md:text-xl uppercase font-semibold text-center">
+          {isHovered ? "As a software Engineer" : "Work experience"}
+        </p>
       </div>
     </div>
   );
