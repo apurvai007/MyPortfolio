54cb0e4105bf39097dd7e09dd55dd30ff389dd54
Apurva Mathur <mathurapurva007@gmail.com>
2024-03-01 16:37:14 +0530

    added email and finalized pic

--- my-portfolio/src/components/AboutCart.jsx
+++ my-portfolio/src/components/AboutCart.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import styles from "@/styles/AboutCard.module.css"
 import Image from "next/image";
-import me from "@/assets/images/me.png";
+import me from "@/assets/images/me3.png";
 
 export default function AboutCard() {
   return (
     <div className="relative col-span-3 aspect-square md:aspect-auto md:row-span-2 bg-[#1c053a9c] dark:bg-[#af72ff56] rounded-3xl overflow-hidden">
       <div className={styles.water}></div>
-      {/* <Image
+      <Image
         src={me}
         alt="Apurva Mathur"
         className="absolute bottom-0 -right-1/4 w-8/12 md:h-full md:auto object-cover -z-0 hidden md:flex"
@@ -17,7 +17,7 @@ export default function AboutCard() {
         placeholder="blur"
         loading="eager"
         priority
-      /> */}
+      />
       <div className="h-full md:w-8/12 absolute top-0 left-0 p-4 md:p-8 flex flex-col justify-start  md:justify-end">
         <h1 className="text-3xl md:text-xl lg:text-5xl font-bold text-white mb-4 md:mb-5">
           👋 {"Hi, I'm"} Apurva Mathur
