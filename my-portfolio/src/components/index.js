a6aabf49ade31609515dd766e58f938e7f5daf13
Apurva Mathur <mathurapurva007@gmail.com>
2024-03-01 16:43:48 +0530

    Added github card

--- my-portfolio/src/components/index.js
+++ my-portfolio/src/components/index.js
@@ -1,9 +1,11 @@
 import AboutCard from "./AboutCart";
 import BirthdayCard from "./BirthdayCard";
 import EmailCard from "./EmailCard";
+import GithubCard from "./GithubCard";
 
 export {
     AboutCard,
     BirthdayCard,
-    EmailCard
+    EmailCard,
+    GithubCard
 };
