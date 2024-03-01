99a74a2bb9c5fb5f20d365a5a606ff11426eab4b
Apurva Mathur <mathurapurva007@gmail.com>
2024-03-01 17:00:11 +0530

    Theme functionality is working

--- my-portfolio/src/components/index.js
+++ my-portfolio/src/components/index.js
@@ -3,11 +3,13 @@ import BirthdayCard from "./BirthdayCard";
 import EmailCard from "./EmailCard";
 import GithubCard from "./GithubCard";
 import LinkedinCard from "./LinkedinCard";
+import ThemeCard from "./ThemeCard";
 
 export {
     AboutCard,
     BirthdayCard,
     EmailCard,
     GithubCard,
-    LinkedinCard
+    LinkedinCard,
+    ThemeCard
 };
